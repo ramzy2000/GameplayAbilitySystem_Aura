39c18c2b55a49f6fd3da0fc384d65a3d0fe6efbc
cduffy-tri <caseyduffy00@gmail.com>
2025-05-17 19:05:26 -0500

    Player and Enemy Characters

---
+++ Source/Aura/Private/Character/AuraCharacterBase.cpp
@@ -0,0 +1,14 @@
+// Copyright Truth Studios
+
+
+#include "Character/AuraCharacterBase.h"
+
+AAuraCharacterBase::AAuraCharacterBase()
+{
+	PrimaryActorTick.bCanEverTick = false;
+}
+
+void AAuraCharacterBase::BeginPlay()
+{
+	Super::BeginPlay();
+}
---
+++ Source/Aura/Private/Character/AuraEnemy.cpp
@@ -0,0 +1,5 @@
+// Copyright Truth Studios
+
+
+#include "Character/AuraEnemy.h"
+
